0d27acde7625c2bf9c1e917af95ca0fb14f5d11d
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-03-30 12:16:48 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home.index')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'title' => 'Accueil'
+        ]);
+    }
+
+    #[Route('/forum', name: 'home.forum')]
+    public function forum(): Response
+    {
+        return $this->render('home/forum.html.twig', [
+            'title' => 'Forum'
+        ]);
+    }
+}
