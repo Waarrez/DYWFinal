a41dabc464c49a993d8ad7d37900dac97d7fb789
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-04-03 16:36:45 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    .addEntry('login', './assets/styles/login.css')
+    .addEntry('main', './assets/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
