0cf58da0cb4c48e9c87ca61928bc5c291b84aadc
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-07-04 16:44:38 +0200

    fix protocol

--- assets/service/Api.js
+++ assets/service/Api.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 const url = window.location.href
 const domain = url.split('/')[2]
+const protocol = window.location.protocol
+
 
 class APIManager {
     static async fetchData(url, options = {}) {
         try {
-            const response = await axios(`http://${domain}${url}`, options)
+            const response = await axios(`${protocol}//${domain}${url}`, options)
             return response.data;
         } catch (error) {
             console.error('API request failed:', error);
