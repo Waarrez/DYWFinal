ea42a29baa524210fce2e47053a34e4542b09250
waarrez <amassya6259///>
2023-11-05 02:36:49 +0100

    fix user (delete verify for this moment)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -61,10 +61,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Groups(["users:read", "users:write"])]
     private Collection $subjects;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(["users:read", "users:write"])]
-    private ?string $isVerify = null;
-
     public function __construct()
     {
         $this->id = Ulid::generate();
@@ -240,18 +236,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $subject->setUsers(null);
             }
         }
-
-        return $this;
-    }
-
-    public function getIsVerify(): ?string
-    {
-        return $this->isVerify;
-    }
-
-    public function setIsVerify(string $isVerify): static
-    {
-        $this->isVerify = $isVerify;
 
         return $this;
     }
